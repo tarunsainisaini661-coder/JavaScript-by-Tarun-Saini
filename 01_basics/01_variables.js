acf19091f047d3a702551030e00178c9ecbc3bac
Tarun saini <tarunsainisaini661@gmail.com>
2026-06-02 06:09:45 +0000

    node 01_basics/01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "tarunsaini661@gmail.com"
+var accountPassword = "859589"
+accountCity = "Jahangirabad"
+let accountState;
+
+// accountId = 2   // not allowed
+
+accountEmail = "usdh8r7t49.com"
+accountPassword = "3y87684589"
+accountCity = "Bulandshahr"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
