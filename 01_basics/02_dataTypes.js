4c09ee6eb5e3e564b5f3eeb0d9e4e68d62652229
Tarun saini <tarunsainisaini661@gmail.com>
2026-06-03 10:24:59 +0000

    update

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3 + 3)   // we are using node.js, not browser
+
+console.log(3
+    +
+     3)  // code readability should be high
+
+console.log("Hitesh");
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boollean => true/false
+// null => standalone value
+// undefined =>
+// Symbol
+
+// Object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // Object
+
+
